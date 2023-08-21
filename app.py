989f48e91b490a2d019e41a587fcf4490bdd4450
wellxie <wellxie@amazon.com>
2023-08-21 23:10:31 +0800

    增加了 产品设计 api

--- app.py
+++ app.py
@@ -26,6 +26,10 @@ llm_predictor = HuggingFacePredictor(
     endpoint_name="chatglm2-lmi-model-2023-08-16-08-23-01-220"
 )
 
+pd_predictor = HuggingFacePredictor(
+    endpoint_name="product-design-sd-2023-08-16-08-18-44-842"
+)
+
 patterns = {
     "redbook": "请根据下面的内容写一段小红书的种草文案: ",
     "zhihu": "请根据下面的内容写一段知乎的文章: ",
@@ -60,5 +64,25 @@ def upload():
 
 
 @app.post("/api/product-design")
-def productDesign():
-    pass
+def productDesign(item: dict):
+    print(item)
+
+    # 这里还需更细致的校验
+    item["steps"] = int(item["steps"])
+    item["seed"] = int(item["seed"])
+    item["height"] = int(item["height"])
+    item["width"] = int(item["width"])
+    item["count"] = int(item["count"])
+
+    # item["steps"] = int(item["steps"])
+    # inputs = {
+    #     "prompt": "3D product render, futuristic armchair, finely detailed, purism, ue 5, a computer rendering, minimalism, octane render, 4k",
+    #     "negative_prompt": "EasyNegative, (worst quality:2), (low quality:2), (normal quality:2), lowres, ((monochrome)), ((grayscale)), cropped, text, jpeg artifacts, signature, watermark, username, sketch, cartoon, drawing, anime, duplicate, blurry, semi-realistic, out of frame, ugly, deformed",
+    #     "steps": 30,
+    #     "sampler": "dpm2_a",
+    #     "seed": -1,
+    #     "height": 512,
+    #     "width": 512,
+    #     "count": 1,
+    # }
+    return pd_predictor.predict(item)
