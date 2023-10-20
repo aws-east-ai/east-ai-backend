012f9f36c776105b30efbbcef60e314019960b82
wellxie <wellxie@amazon.com>
2023-10-20 15:20:52 +0800

    add bedrock fix char error

--- controller/websocket.py
+++ controller/websocket.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
-from io import BytesIO, SEEK_END
 import json
 import boto3
 import os
@@ -9,8 +8,8 @@ ws_router = APIRouter()
 
 region = os.environ.get("AWS_DEFAULT_REGION", "us-west-2")
 smr = boto3.client("sagemaker-runtime", region_name=region)
+bedrock = boto3.client(service_name="bedrock-runtime", region_name=region)
 
-parameters = {"max_length": 4092, "temperature": 0.01, "top_p": 0.8}
 patterns = {
     "redbook": "你是一个时尚的年轻人，喜欢用emoji，请根据下面的内容写一段小红书的种草文案: ",
     "zhihu": "你是一个知识博学的学者，请根据下面的内容写一段文章，发表在知乎上: ",
@@ -21,27 +20,8 @@ patterns = {
     "zhidemai": "你是一个经验丰富的导购，请根据下面的内容写一段值得买的文章: ",
     "douyin": "你是一个短片导演，请根据下面的内容写一段抖音的拍摄剧本: ",
     "kuaishou": "你是一个短片导演，请根据下面的内容写一段快手的短片剧本: ",
+    "freestyle": "",
 }
-
-
-class StreamScanner:
-    def __init__(self):
-        self.buff = BytesIO()
-        self.read_pos = 0
-
-    def write(self, content):
-        self.buff.seek(0, SEEK_END)
-        self.buff.write(content)
-
-    def readlines(self):
-        self.buff.seek(self.read_pos)
-        for line in self.buff.readlines():
-            if line[-1] != b"\n":
-                self.read_pos += len(line)
-                yield line[:-1]
-
-    def reset(self):
-        self.read_pos = 0
 
 
 @ws_router.websocket("/api/chat-bot")
@@ -53,50 +33,94 @@ async def chat_bot(websocket: WebSocket):
 
             # TODO: error handle，如果不是 json 格式则会报错
             item = json.loads(data)
+            model_id = item["model_id"] if "model_id" in item else "chatglm2"
+
             history = item["history"] if "history" in item else []
+
             pattern = item["pattern"]
             prompt_pattern = patterns[pattern]
             prompt = item["prompt"]
             prompt = prompt if history else prompt_pattern + "\n\n" + prompt
-
             question = {"status": "begin", "question": prompt}
             await websocket.send_text(json.dumps(question, ensure_ascii=False))
 
-            response_model = smr.invoke_endpoint_with_response_stream(
-                EndpointName="chatglm2-lmi-model",
-                Body=json.dumps(
-                    {"inputs": prompt, "parameters": parameters, "history": history}
-                ),
-                ContentType="application/json",
-            )
-
-            event_stream = response_model["Body"]
-            scanner = StreamScanner()
-            resp = {}
-            for event in event_stream:
-                scanner.write(event["PayloadPart"]["Bytes"])
-                for line in scanner.readlines():
-                    try:
-                        resp = json.loads(line)["outputs"]
-                        await websocket.send_text(resp["outputs"])
-                    except Exception as e:
-                        continue
-            result_end = (
-                '{"status": "done", "history": '
-                + json.dumps(resp["history"], ensure_ascii=False)
-                + "}"
-            )
-            await websocket.send_text(result_end)
+            if model_id == "chatglm2":
+                await ask_chatglm2(websocket, prompt, history)
+            elif model_id == "bedrock_claude2":
+                await ask_bedrock_claude2(websocket, prompt, history)
     except WebSocketDisconnect:
         print(f"Client left")
 
 
-# @app.post("/api/write-marketing-text")
-# def write_marketing_text(item: dict):
-#     history = item["history"] if "history" in item else []
-#     pattern = item["pattern"]
-#     prompt_pattern = patterns[pattern]
-#     prompt = item["prompt"]
-#     prompt = prompt if history else prompt_pattern + "\n\n" + prompt
-#     res = llm_predictor.predict({"inputs": prompt, "parameters": {"history": history}})
-#     return res
+async def ask_chatglm2(websocket: WebSocket, prompt: str, history):
+    parameters = {"max_length": 4092, "temperature": 0.01, "top_p": 0.8}
+
+    response_model = smr.invoke_endpoint_with_response_stream(
+        EndpointName="chatglm2-lmi-model",
+        Body=json.dumps(
+            {"inputs": prompt, "parameters": parameters, "history": history}
+        ),
+        ContentType="application/json",
+    )
+    stream = response_model.get("Body")
+    if stream:
+        chunk_str_full = ""
+        for event in stream:
+            chunk = event.get("PayloadPart")
+            if chunk:
+                chunk_str_full = chunk_str_full + chunk.get("Bytes").decode()
+                # print(chunk_str_full)
+                if chunk_str_full.strip().endswith(
+                    "]}}"
+                ) and chunk_str_full.strip().startswith("{"):
+                    chunk_obj = json.loads(chunk_str_full)
+                    result = chunk_obj["outputs"]["outputs"]
+                    chunk_str_full = ""
+                    await websocket.send_text(result)
+
+    result_end = '{"status": "done"}'
+    await websocket.send_text(result_end)
+
+
+async def ask_bedrock_claude2(websocket: WebSocket, prompt: str, history):
+    modelId = "anthropic.claude-v2"
+    accept = "*/*"
+    contentType = "application/json"
+    body = json.dumps(
+        {
+            "prompt": claude2_combine_history(history, prompt),
+            "max_tokens_to_sample": 2048,
+            "temperature": 0.5,
+            "top_p": 0.9,
+        }
+    )
+    response = bedrock.invoke_model_with_response_stream(
+        body=body,
+        modelId=modelId,
+        accept=accept,
+        contentType=contentType,
+    )
+    stream = response.get("body")
+    if stream:
+        for event in stream:
+            chunk = event.get("chunk")
+            if chunk:
+                chunk_obj = json.loads(chunk.get("bytes").decode())
+                # print(chunk_obj)
+                text = chunk_obj["completion"]
+                await websocket.send_text(text)
+
+        result_end = '{"status": "done"}'
+        await websocket.send_text(result_end)
+
+
+def claude2_combine_history(history, newQ):
+    if not history:
+        return "Human:{prompt} \\n\\nAssistant:".format(prompt=newQ)
+
+    prompt = ""
+    for [q, a] in history:
+        prompt = prompt + "Human:{q}\\n\\nAssistant:{a}\\n\\n".format(q=q, a=a)
+    prompt = prompt + "Human:{prompt} \\n\\nAssistant:".format(prompt=newQ)
+    # print(prompt)
+    return prompt
