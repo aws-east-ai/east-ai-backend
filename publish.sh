17e581181ed982af901b7a802b604df0c5db3b95
wellxie <wellxie@amazon.com>
2024-03-11 17:43:57 +0800

    modify publish script

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ rm -rf ../east-ai-deploy/dist/east-ai-backend
 git clone https://github.com/aws-east-ai/east-ai-backend ../east-ai-deploy/dist/east-ai-backend
 rm -rf ../east-ai-deploy/dist/east-ai-backend/.git
 
-cd ../east-ai-deploy/dist/east-ai-backend
-git add .
-git commit -m "update"
-git push
+# cd ../east-ai-deploy/dist/east-ai-backend
+# git add .
+# git commit -m "update"
+# git push
